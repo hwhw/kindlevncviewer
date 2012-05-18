46adb803c4b49920bad64eaafcdd30d6b670f1c4
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-05-18 21:42:12 +0200

    use com.lab126.powerd to preventScreenSaver
    This is (IMHO) nicer than using third-party tool which users have
    to install

--- kvncviewer.sh
+++ kvncviewer.sh
@@ -2,10 +2,7 @@
 cd $(dirname "$0")
 LD_LIBRARY_PATH=.
 export LD_LIBRARY_PATH
-## uncomment the calls to kaffeine to make your reader stay awake.
-## needs the binary of kaffeine and expects it in kindlevncviewers directory.
-## info on kaffeine & download:
-## http://www.mobileread.com/forums/showthread.php?t=151207
-# ./kaffeine
+
+lipc-set-prop com.lab126.powerd preventScreenSaver 1
 ./kindlevncviewer -config config.lua "$@"
-# ./kaffeine 3
+lipc-set-prop com.lab126.powerd preventScreenSaver 0
