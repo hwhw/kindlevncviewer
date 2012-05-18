31c28a0bcbe98cb0db1ca4d255fda7c23c087260
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-05-18 22:00:30 +0200

    on exit send menu key twice to refresh display

--- kvncviewer.sh
+++ kvncviewer.sh
@@ -6,3 +6,7 @@ export LD_LIBRARY_PATH
 lipc-set-prop com.lab126.powerd preventScreenSaver 1
 ./kindlevncviewer -config config.lua "$@"
 lipc-set-prop com.lab126.powerd preventScreenSaver 0
+
+# send menu key twice to refresh display
+echo "send 139" > /proc/keypad
+echo "send 139" > /proc/keypad
